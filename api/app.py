5c5c7938db3238d7527cb6cf9d0657568efc522b
Matheus H. Lougen <matheus.lougen@gmail.com>
2024-12-22 00:33:31 -0300

    feat: add schemas for message responses

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from typing import Any
-
 from fastapi import FastAPI
+
+from api.schemas import Message
 
 app = FastAPI()
 
 
 @app.get('/')
-def root() -> Any:
+def root() -> Message:
     return {'message': 'Hello World'}
