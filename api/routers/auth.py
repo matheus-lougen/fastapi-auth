f0d39c2682c99b6ecf8a1518dfe3f7f0d4b78190
Matheus H. Lougen <matheus.lougen@gmail.com>
2025-02-27 21:43:45 -0300

    refactor: create routers for user and auth operations

---
+++ api/routers/auth.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from api import models, security
+from api.database import Session, get_session
+
+router = APIRouter('/auth', tags=['auth'])
+
+
+@router.post('/token/', status_code=status.HTTP_200_OK)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)) -> Any:
+    user = models.User.fetch_by_email(form_data.username, session)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Incorrect email')
+
+    if not security.verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Incorrect password')
+
+    access_token = security.create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
---
+++ api/routers/users.py
@@ -0,0 +1,74 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, IntegrityError, status
+from sqlalchemy import select
+
+from api import models, schemas, security
+from api.database import Session, get_session
+
+router = APIRouter(prefix='/users', tags=['users'])
+
+
+@router.post('/', response_model=schemas.PublicUser, status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.User, session: Session = Depends(get_session)) -> Any:
+    user = models.User(username=user.username, password=security.get_password_hash(user.password), email=user.email)
+
+    if user.exists(session):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail='Esse nome de usuário ou e-mail já estão em uso!'
+        )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
+
+
+@router.get('/', response_model=schemas.UserList, status_code=status.HTTP_200_OK)
+def fetch_all_users_from_database(skip: int = 0, limit: int = 100, session: Session = Depends(get_session)) -> Any:
+    users = session.scalars(select(models.User).offset(skip).limit(limit)).all()
+    return {'users': users}
+
+
+@router.get('/{user_id}', response_model=schemas.PublicUser, status_code=status.HTTP_302_FOUND)
+def fetch_user_from_database(user_id: int, session: Session = Depends(get_session)) -> Any:
+    user = models.User.fetch_by_id(user_id, session)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    return user
+
+
+@router.put('/{user_id}', response_model=schemas.PublicUser, status_code=status.HTTP_200_OK)
+def update_user_info(
+    user_id: int,
+    user: schemas.User,
+    session: Session = Depends(get_session),
+    current_user: models.User = Depends(security.get_current_user),
+) -> Any:
+    if current_user.id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not enough permissions')
+
+    current_user.username = user.username
+    current_user.password = security.get_password_hash(user.password)
+    current_user.email = user.email
+
+    try:
+        session.commit()
+        session.refresh(current_user)
+    except IntegrityError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='This username or email is alreadly in use')
+    return current_user
+
+
+@router.delete('/{user_id}', response_model=schemas.Message, status_code=status.HTTP_200_OK)
+def delete_user_from_database(
+    user_id: int, session: Session = Depends(get_session), current_user: models.User = Depends(security.get_current_user)
+) -> schemas.Message:
+    if current_user.id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not enough permissions')
+
+    session.delete(current_user)
+    session.commit()
+
+    return {'message': 'User deleted'}
